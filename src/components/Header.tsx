2f9ec3191b1616d2a4ffe1bbd1243cc03ae6b161
y9tr3ble <tr3ble@outlook.com>
2025-01-17 13:38:00 +0300

    Refactor Header component to use Next.js Link for navigation
    - Replaced static text links with Next.js Link components for improved navigation.
    - Added hover effects and transition styles to enhance user experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function Header() {
 	return (
 		<header className="flex justify-between items-center p-8 shadow-md">
@@ -5,10 +7,18 @@ function Header() {
 				<p className="text-xl font-bold">Tr3ble</p>
 			</div>
 			<div className="flex space-x-4 pr-10">
-				<p className="text-xl font-bold">Home</p>
-				<p className="text-xl font-bold">Blog</p>
-				<p className="text-xl font-bold">About</p>
-				<p className="text-xl font-bold">Skills</p>
+				<Link href="/" className="text-xl font-bold hover:text-purple-600 transition-colors duration-500">
+					Home
+				</Link>
+				<Link href="/blog" className="text-xl font-bold hover:text-purple-600 transition-colors duration-500">
+					Blog
+				</Link>
+				<Link href="/about" className="text-xl font-bold hover:text-purple-600 transition-colors duration-500">
+					About
+				</Link>
+				<Link href="/skills" className="text-xl font-bold hover:text-purple-600 transition-colors duration-500">
+					Skills
+				</Link>
 			</div>
 		</header>
 	);
