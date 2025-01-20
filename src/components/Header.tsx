ede3f34bc20a3431d4caf6745871e6ca6d3dcd0d
y9tr3ble <tr3ble@outlook.com>
2025-01-20 12:35:50 +0300

    Refactor Header component padding for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import MobileMenu from "./MobileMenu"
 function Header() {
   return (
     <header className="flex justify-between items-center p-5 shadow-md">
-      <div className="pl-10 lg:pl-48 md:flex">
+      <div className="pl-2 lg:pl-48 md:flex">
         <p className="text-2xl font-bold">Tr3ble</p>
       </div>
       <div className="hidden md:flex space-x-4 pr-10 lg:pr-48">
