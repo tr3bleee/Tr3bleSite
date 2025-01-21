e31560469ae4430efa36ce5a21d41cad28a15207
y9tr3ble <tr3ble@outlook.com>
2025-01-21 14:54:40 +0300

    Added tooltip

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -5,8 +5,14 @@ import {
 	SiTailwindcss,
 	SiFastify,
 	SiPrisma,
-	SiPostgresql,
+	SiJavascript,
 } from "react-icons/si";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 function TechStack() {
 	return (
@@ -14,13 +20,64 @@ function TechStack() {
 			<p className="font-bold text-xl">TechStack</p>
 			{/* logos in row */}
 			<div className="flex flex-row gap-4">
-				<SiTypescript size={30} />
-				<SiReact size={30} />
-				<SiNextdotjs size={30} />
-				<SiTailwindcss size={30} />
-				<SiFastify size={30} />
-				<SiPrisma size={30} />
-				<SiPostgresql size={30} />
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiJavascript size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>JavaScript</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiTypescript size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>TypeScript</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiReact size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>React</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiNextdotjs size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>Next.js</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiTailwindcss size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>TailwindCSS</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiFastify size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>Fastify</p>
+					</TooltipContent>
+				</Tooltip>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<SiPrisma size={30} />
+					</TooltipTrigger>
+					<TooltipContent>
+						<p>Prisma</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 			</div>
 		</div>
 	);
