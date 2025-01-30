6bb9e5a0f65d59117f2fe7861c9bc8d40e27811e
y9tr3ble <tr3ble@outlook.com>
2025-01-30 19:06:25 +0300

    optimize fonts and margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
 		<>
 			<Header />
 			<main className="flex min-h-screen animate-fade-in">
-				<div className="flex flex-col ml-10 md:ml-48 mt-44">
+				<div className="flex flex-col ml-6 md:ml-52 mt-48">
 					<div className="gap-12">
-						<h1 className="text-5xl font-bold md:text-6xl">
+						<h1 className="text-5xl font-bold md:text-7xl">
 							Hi, <br /> I am <span className="bg-gradient-to-r from-cyan-200 to-violet-600 bg-clip-text text-transparent">Tr3ble</span>
 						</h1>
-						<h2 className="text-2xl font-bold text-gray-600 dark:text-gray-400">
+						<h2 className="text-2xl md:text-4xl font-bold text-gray-600 dark:text-gray-400">
 							<TypeAnimation
 								sequence={[
 									"i am a Full-Stack Developer",
