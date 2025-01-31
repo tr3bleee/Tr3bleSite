6f329f1b4581cc53a7f39434ee7de2c1a6a6a19b
y9tr3ble <tr3ble@outlook.com>
2025-02-01 00:52:35 +0300

    fix footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ import Terminal from "@/components/terminal/terminal";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-hidden flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Header />
-      <main className="flex-1 flex animate-fade-in flex-col lg:flex-row items-start justify-center lg:justify-between px-2 sm:px-4 md:px-20 xl:px-32 pt-20 lg:pt-36">
+      <main className="flex-1 min-h-screen animate-fade-in flex flex-col lg:flex-row items-start justify-center lg:justify-between px-2 sm:px-4 md:px-20 xl:px-32 pt-20 lg:pt-36">
         {/* Left Column */}
         <div className="flex flex-col w-full lg:max-w-[50%] mb-12 lg:mb-0 lg:mr-14">
           <h1 className="text-3xl font-bold md:text-6xl lg:mt-10 ml-1 sm:ml-0">
