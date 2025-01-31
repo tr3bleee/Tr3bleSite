98a1c1739b95d38506133cf188aeed301499f4b3
y9tr3ble <tr3ble@outlook.com>
2025-02-01 00:42:47 +0300

    fix icon flick

--- src/components/ThemeToogle.tsx
+++ src/components/ThemeToogle.tsx
@@ -1,18 +1,25 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { MoonIcon, SunIcon } from 'lucide-react';
 
 export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme, systemTheme } = useTheme();
+  
   const currentTheme = theme === 'system' ? systemTheme : theme;
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   const toggleTheme = () => {
-    if (currentTheme === 'dark') {
-      setTheme('light');
-    } else {
-      setTheme('dark');
-    }
+    setTheme(currentTheme === 'dark' ? 'light' : 'dark');
   };
 
   return (
