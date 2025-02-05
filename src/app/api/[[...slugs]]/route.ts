9af4d2afaec21496ceaf4ad295cf5bbc3ff411b7
y9tr3ble <tr3ble@outlook.com>
2025-02-05 14:28:20 +0300

    Revert "Fix swagger"
    This reverts commit a816a2c5e55868f48bd77bffe90b5486425f8f16.

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,7 +1,9 @@
-import { app } from "@/app/api/[[...slugs]]/core";
+import {app} from "@/app/api/[[...slugs]]/core";
 
 const mainApp = app
+    .get('/api', () => 'hi')
+
+export const dynamic = 'force-dynamic'
 
 export const GET = (request: Request) => mainApp.handle(request)
 export const POST = (request: Request) => mainApp.handle(request)
-export const dynamic = 'force-dynamic'
--- src/app/swagger/json/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function SwaggerPage() {
-    redirect("/api/swagger/json");
-}
